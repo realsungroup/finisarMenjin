7a13fdfc54ec37640c7d460eaca48debbb7be0fa
hileix <304192604@qq.com>
2018-11-25 11:33:00 +0800

    fix(App.js) 修复顶部按钮的样式问题

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Table, Divider, Button, Input, message, Popconfirm } from "antd";
-import "antd/dist/antd.css";
-import { setItem } from "../src/util/localCache";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Table, Divider, Button, Input, message, Popconfirm } from 'antd';
+import 'antd/dist/antd.css';
+import { setItem } from '../src/util/localCache';
 import {
   getData,
   getDataByVal,
   modRecord,
   modRecords,
   defaultLogin
-} from "../src/util/api";
-import exportJsonExcel from "js-export-excel";
+} from '../src/util/api';
+import exportJsonExcel from 'js-export-excel';
 // import { NonsupportIE } from "nonsupport-ie-react";
 const { Column, ColumnGroup } = Table;
 
@@ -21,7 +21,7 @@ const rowSelection = {
   onChange: (selectedRowKeys, selectedRows) => {
     console.log(
       `selectedRowKeys: ${selectedRowKeys}`,
-      "selectedRows: ",
+      'selectedRows: ',
       selectedRows
     );
   },
@@ -41,7 +41,7 @@ class App extends Component {
 
   //截取地址栏参数
   GetQueryString = name => {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return unescape(r[2]);
     return null;
@@ -49,18 +49,18 @@ class App extends Component {
 
   //登陆
   login = async () => {
-    const code = this.GetQueryString("Code");
-    const pass = this.GetQueryString("Password");
+    const code = this.GetQueryString('Code');
+    const pass = this.GetQueryString('Password');
     const response = await defaultLogin(code, pass);
     const result = response.OpResult;
-    if (result === "Y") {
-      setItem("userInfo", JSON.stringify(response));
-      message.success("登陆成功");
+    if (result === 'Y') {
+      setItem('userInfo', JSON.stringify(response));
+      message.success('登陆成功');
       this.loadTasks();
       this.setState({
         show: false
       });
-    } else if (result === "N") {
+    } else if (result === 'N') {
       const errorMsg = response.ErrorMsg;
       message.error(errorMsg);
     }
@@ -99,13 +99,13 @@ class App extends Component {
 
   getTableData = async (
     options = Object.assign(this.state.pagination || {}, {
-      key: ""
+      key: ''
     }),
     cmswhere
   ) => {
     let res;
     const { resid, cmscolumns } = this.props;
-    cmswhere = `${cmswhere ? cmswhere + " and " : ""} ${this.props.cmswhere}`;
+    cmswhere = `${cmswhere ? cmswhere + ' and ' : ''} ${this.props.cmswhere}`;
 
     try {
       res = await getDataByVal(595199561778, options.key);
@@ -115,7 +115,7 @@ class App extends Component {
         const predata = [];
         res.data.map(val => {
           predata.push(val);
-          predata["checked"] = true;
+          predata['checked'] = true;
         });
         await this.setState({
           data: predata,
@@ -150,7 +150,7 @@ class App extends Component {
     this.getTableData(Object.assign(o, { key: value }));
   };
   rightconfirm = async () => {
-    this.state.data.forEach(item => (item.C3_595192402751 = "Y"));
+    this.state.data.forEach(item => (item.C3_595192402751 = 'Y'));
     let res;
     try {
       res = await modRecords(595199561778, this.state.data);
@@ -160,7 +160,7 @@ class App extends Component {
     if (!res.data.length) {
       return message.error(res.message);
     }
-    message.success("操作成功");
+    message.success('操作成功');
     this.loadTasks();
   };
 
@@ -168,9 +168,9 @@ class App extends Component {
     this.setState({
       columns: [
         {
-          title: "姓名",
-          dataIndex: "C3_595166693246",
-          key: "C3_595166693246",
+          title: '姓名',
+          dataIndex: 'C3_595166693246',
+          key: 'C3_595166693246',
           // filters: this.state.filter,
           filterMultiple: false,
           onFilter: (value, record) =>
@@ -179,9 +179,9 @@ class App extends Component {
           render: text => <a href="javascript:;">{text}</a>
         },
         {
-          title: "工号",
-          dataIndex: "C3_595166604634",
-          key: "C3_595166604634",
+          title: '工号',
+          dataIndex: 'C3_595166604634',
+          key: 'C3_595166604634',
 
           filterMultiple: true,
           onFilter: (value, record) =>
@@ -189,9 +189,9 @@ class App extends Component {
           sorter: (a, b) => a.C3_595166604634.length - b.C3_595166604634.length
         },
         {
-          title: "部门",
-          dataIndex: "C3_595166712341",
-          key: "C3_595166712341",
+          title: '部门',
+          dataIndex: 'C3_595166712341',
+          key: 'C3_595166712341',
 
           filterMultiple: true,
           onFilter: (value, record) =>
@@ -199,9 +199,9 @@ class App extends Component {
           sorter: (a, b) => a.C3_595166712341.length - b.C3_595166712341.length
         },
         {
-          title: "所属门权限组名称",
-          dataIndex: "C3_595166751093",
-          key: "C3_595166751093",
+          title: '所属门权限组名称',
+          dataIndex: 'C3_595166751093',
+          key: 'C3_595166751093',
 
           filterMultiple: true,
           onFilter: (value, record) =>
@@ -209,9 +209,9 @@ class App extends Component {
           sorter: (a, b) => a.C3_595166751093.length - b.C3_595166751093.length
         },
         {
-          title: "管理员工号",
-          dataIndex: "C3_595166775274",
-          key: "C3_595166775274",
+          title: '管理员工号',
+          dataIndex: 'C3_595166775274',
+          key: 'C3_595166775274',
 
           filterMultiple: true,
           onFilter: (value, record) =>
@@ -219,9 +219,9 @@ class App extends Component {
           sorter: (a, b) => a.C3_595166775274.length - b.C3_595166775274.length
         },
         {
-          title: "管理员姓名",
-          dataIndex: "C3_595166794150",
-          key: "C3_595166794150",
+          title: '管理员姓名',
+          dataIndex: 'C3_595166794150',
+          key: 'C3_595166794150',
 
           filterMultiple: true,
           onFilter: (value, record) =>
@@ -229,9 +229,9 @@ class App extends Component {
           sorter: (a, b) => a.C3_595166794150.length - b.C3_595166794150.length
         },
         {
-          title: "是否新增",
-          dataIndex: "C3_595168410919",
-          key: "C3_595168410919",
+          title: '是否新增',
+          dataIndex: 'C3_595168410919',
+          key: 'C3_595168410919',
 
           filterMultiple: true,
           onFilter: (value, record) =>
@@ -239,9 +239,9 @@ class App extends Component {
           sorter: (a, b) => a.C3_595168410919.length - b.C3_595168410919.length
         },
         {
-          title: "确认无误",
-          dataIndex: "C3_595192402751",
-          key: "C3_595192402751",
+          title: '确认无误',
+          dataIndex: 'C3_595192402751',
+          key: 'C3_595192402751',
 
           filterMultiple: true,
           onFilter: (value, record) =>
@@ -288,7 +288,7 @@ class App extends Component {
     const { selectedRowKeys, data } = this.state;
 
     if (!selectedRowKeys.length) {
-      return message.error("请先勾选记录");
+      return message.error('请先勾选记录');
     }
     let filteredData = [];
     selectedRowKeys.forEach(key => {
@@ -299,7 +299,7 @@ class App extends Component {
     });
 
     filteredData = JSON.parse(JSON.stringify(filteredData));
-    filteredData.forEach(item => (item["C3_595192402751"] = "N"));
+    filteredData.forEach(item => (item['C3_595192402751'] = 'N'));
     let res;
     try {
       res = await modRecords(595199561778, filteredData);
@@ -308,11 +308,11 @@ class App extends Component {
     }
 
     if (!res.data.length) {
-      return message.error("操作失败");
+      return message.error('操作失败');
     }
     this.loadTasks();
     this.setState({ selectedRowKeys: [] });
-    message.success("操作成功");
+    message.success('操作成功');
   };
 
   render() {
@@ -328,15 +328,17 @@ class App extends Component {
       // >
       <div className="App">
         <div className="btns">
-          <Search
-            style={{ width: "10%" }}
-            onSearch={this.search}
-            enterButton
-            disabled={show}
-            // value={value}
-            onChange={() => {}}
-          />
-          <div>
+          <div style={{ float: 'left' }}>
+            <Search
+              style={{ width: 200 }}
+              onSearch={this.search}
+              enterButton
+              disabled={show}
+              // value={value}
+              onChange={() => {}}
+            />
+          </div>
+          <div style={{ float: 'right' }}>
             {show ? (
               <Button
                 className="btn-approve"
@@ -374,7 +376,7 @@ class App extends Component {
               onCancel={this.cancel}
               onConfirm={this.rightconfirm}
             >
-              {" "}
+              {' '}
               <Button
                 className="btn-reject"
                 type="primary"
@@ -383,7 +385,7 @@ class App extends Component {
                 // loading={loading}
               >
                 全部无误
-              </Button>{" "}
+              </Button>{' '}
             </Popconfirm>
 
             <Button
